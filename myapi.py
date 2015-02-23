ab07a63a7bb5ab514b734f75142d00a5b6a9096f
Chris Villa <chrisvilla@me.com>
2015-02-23 16:31:02 +0000

    commented out endpoint docs and tweaks functional_method

--- myapi.py
+++ myapi.py
@@ -1,30 +1,4 @@
-def restful_method(id):
-    """
-    @api {get} /user/:id Request User information
-    @apiName GetUser
-    @apiGroup TestApi
 
-    @apiParam {Number} id Users unique ID.
-
-    @apiExample {shell} Example usage:
-        curl -i http://localhost/user/4711
-
-    @apiSuccessExample {json} 200 Success-Response
-        HTTP/1.1 200 OK
-        {
-            "firstname": "John",
-            "lastname": "Doe"
-        }
-
-    @apiErrorExample {json} 404 Error-Response
-        HTTP/1.1 404 Not Found
-        {
-            "error": "UserNotFound"
-        }
-
-    """
-
-    return None
 
 
 def functional_method(first_arg, second_arg='Test'):
@@ -39,7 +13,7 @@ def functional_method(first_arg, second_arg='Test'):
     @apiExample {python} Example usage:
         functional_method(5, "This is cool")
 
-    @apiSuccessExample {json} Success-Response
+    @apiSuccessExample {json}
         {
             "firstname": 5,
             "lastname": "This is cool"
