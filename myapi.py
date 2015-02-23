eac93d1153b0ee6c9a60baa5e0fc98c3835d9a7c
Chris Villa <chrisvilla@me.com>
2015-02-23 16:18:57 +0000

    functional_method api test

--- myapi.py
+++ myapi.py
@@ -1,8 +1,8 @@
-def do_some_stuff(first_arg, second_arg='Test'):
+def restful_method(id):
     """
     @api {get} /user/:id Request User information
     @apiName GetUser
-    @apiGroup User
+    @apiGroup TestApi
 
     @apiParam {Number} id Users unique ID.
 
@@ -25,3 +25,29 @@ def do_some_stuff(first_arg, second_arg='Test'):
     """
 
     return None
+
+
+def functional_method(first_arg, second_arg='Test'):
+    """
+    @api {fn()} functional_method Functional Method
+    @apiName FunctionalMethod
+    @apiGroup TestApi
+
+    @apiParam {Number} first_arg Some number.
+    @apiParam {String} [second_arg] Some string.
+
+    @apiExample {python} Example usage:
+        functional_method(5, "This is cool")
+
+    @apiSuccessExample {json} Success
+        {
+            "firstname": 5,
+            "lastname": "This is cool"
+        }
+
+    """
+
+    return {
+        'first_arg': first_arg,
+        'second_arg': second_arg
+    }
